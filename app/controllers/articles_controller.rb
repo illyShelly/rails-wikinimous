8f416556417a7e0a653055c22602c00688f806c0
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-02-10 17:52:58 +0100

    CRUD faker seed

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,59 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  # CRUD
+  # GET '/articles'
+  def index
+    @articles = Article.all
+  end
+
+  # GET /articles/1
+  def show
+  end
+
+  # GET /articles/new
+  def new
+    @article = Article.new
+  end
+
+  # GET /articles/1/edit
+  def edit
+  end
+
+  # POST /articles
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article, notice: 'Article was successfully created :)'
+    else
+      render :new
+    end
+  end
+
+  # PATH/PUT /articles/1
+  def update
+    if @article.update(article_params)
+      redirect_to @article, notice: 'Article was successfully updated :)'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /articles/1
+  def destroy
+    @article.destroy
+    redirect_to root_path, notice: "Article was destroyed"
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_article
+      @article = Article.find(params[:id])
+    end
+    def article_params
+      # *Strong params*: You need to *whitelist* what can be updated by the user
+      # Never trust user data!
+      params.require(:article).permit(:title, :content)
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# using CREATE or variable => NEW, then SAVE method
+Article.destroy_all
+
+puts "Creating 20 fake articles"
+20.times do
+  article = Article.new(
+    title: Faker::Company.catch_phrase,
+    content: Faker::Lorem.sentences(15)
+    )
+  article.save!
+end
+puts "Finished seed"
+
+
+# as sentences: -> get rid off arrays brackets
+
+# Advanced hybrid superstructure
+# ["Quia at sed.", "Aperiam sit suscipit.", "Quae modi autem."]
+# Public-key value-added contingency
+# Omnis est blanditiis.Quis aut voluptate.Qui voluptatem cum.
+
+# Article.destroy_all
+# def fake_content
+#   paragraphs = []
+#   paragraphs << Faker::Lorem.paragraph
+#   paragraphs << "![](http://lorempixel.com/800/300/city/)"
+#   paragraphs << "## #{Faker::Company.catch_phrase}"
+#   paragraphs << Faker::Lorem.paragraphs(2)
+#   paragraphs << "**#{Faker::Lorem.word}** #{Faker::Lorem.sentences.join}"
+#   paragraphs << "## #{Faker::Company.catch_phrase}"
+#   paragraphs << "#{Faker::Lorem.sentences.join} [#{Faker::Commerce.product_name}](#{Faker::Internet.url}) #{Faker::Lorem.sentences.join}"
+#   paragraphs << Faker::Lorem.paragraphs(3)
+#   paragraphs.flatten.join("\n\n")
+# end
+
+# 20.times do
+#   Article.create(
+#     title: Faker::Company.catch_phrase,
+#     content: fake_content
+#   )
+# end
